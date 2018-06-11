b9a541125ec9e353632be9a12c7e57a7f5d8a75b
snowxueyi <1391964315@qq.com>
2018-06-11 10:17:04 +0800

    页面引入相应组件

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import Card from '@/components/Card'
 import Fuzzy from '@/components/Fuzzy'
 import EditableFuzzy from '@/components/EditableFuzzy'
 import EditableInput from '@/components/EditableInput'
+import Frame from '@/components/Frame';
+import FrameWarp from '@/components/FrameWarp';
+import Aside from '@/components/com/Aside';
+import Head from '@/components/com/Head';
 
 
-
-let kzui = { Card,Fuzzy,EditableFuzzy,EditableInput }
+let kzui = { Card,Fuzzy,EditableFuzzy,EditableInput,Frame,FrameWarp,Aside,Head }
 const install = () => {
   for (let k in kzui) {
     window.Vue.component(kzui[k].name, kzui[k]);
@@ -13,4 +16,3 @@ const install = () => {
 }
 (typeof window !== 'undefined' && window.Vue && install()) || (kzui.install = install)
 export default kzui;
-
