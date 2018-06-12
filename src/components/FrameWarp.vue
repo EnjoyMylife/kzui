f751ad402bffcdd0a0fb591239801dfd4747f5af
snowxueyi <1391964315@qq.com>
2018-06-12 10:36:01 +0800

    页面顶部tab栏添加

--- src/components/FrameWarp.vue
+++ src/components/FrameWarp.vue
@@ -1,12 +1,36 @@
 <template>
 <div class="kz-content-warp" >
+  <kz-tabs v-model="tabIndex" closable @tab-remove="removeTab">
+    <kz-tab-panel
+      v-for="item in tabData"
+      :key="item.name"
+      :label="item.title"
+      :name="item.name"
+    >
+      {{item.content}}
+    </kz-tab-panel>
+  </kz-tabs>
+  <div >
+    <el-button
+      size="small"
+      @click="addTab(tabIndex)"
+    >
+      add tab
+    </el-button>
+    <el-button
+      size="small"
+      @click="handleSwitch(2)"
+    >
+      switch tab2
+    </el-button>
+  </div>
 	<section class="kz-content-header">
 		<h1>
 			{{title}}
 		</h1>
-        <slot name="header"></slot>
+    <!-- <slot name="header"></slot> -->
 	</section>
-	<section class="kz-content">
+	<section class="kz-tab1-content">
 		<slot></slot>
 	</section>
 </div>
@@ -15,7 +39,42 @@
 <script>
 export default {
   name:'kz-frame-warp',
-  props: ["title"]
+  props: ["title", "tabData", "tabIndex", "currentTab"],
+  methods:{
+    removeTab: function (targetName) {
+      let tabs = this.tabsData;
+        let activeName = this.tabIndex;
+        if (activeName === targetName) {
+          tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+              let nextTab = tabs[index + 1] || tabs[index - 1];
+              if (nextTab) {
+                activeName = nextTab.name;
+              }
+            }
+          });
+        }
+      console.log(activeName)
+      this.tabIndex = activeName;
+      this.tabsData = tabs.filter(tab => tab.name !== targetName);
+    },
+    addTab(targetName) {
+        let newTabName = ++this.currentTab + '';
+        this.tabsData.push({
+          title: 'New Tab',
+          name: newTabName,
+          content: 'New Tab content'
+        });
+        this.tabIndex = newTabName;
+      },
+      handleClick() {
+        alert('this btn has been clicked.')
+      },
+      handleSwitch(tabindex) {
+        console.log(tabindex)
+        this.tabIndex = tabindex.toString()
+      }
+  }
 };
 </script>
 <style >
@@ -32,16 +91,10 @@ export default {
 .kz-content-header {
   position: relative;
   padding: 15px 15px 0 15px;
+  background-color: #fff;
 }
 
-.kz-content {
-  position: relative;
-  border-radius: 3px;
+.kz-tab1-content {
   background: #ffffff;
-  border-top: 3px solid #d2d6de;
-  margin-bottom: 20px;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-  margin: 15px;
-  padding: 15px;
 }
 </style>
